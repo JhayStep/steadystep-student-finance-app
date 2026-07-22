8ef5b70b8520061ab0ae2739e235824c4c296fdf
jhaystep <Ballislife322004@gmail.com>
2026-07-22 15:36:02 -0700

    Add React Router and project routing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,18 @@
-import './App.css'
-import Home from './pages/Home'
+import Login from "./pages/Login"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./App.css"
+
+import Home from "./pages/Home"
 
 function App() {
-  return <Home />
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
---
+++ client/src/pages/Login.tsx
@@ -0,0 +1,10 @@
+function Login() {
+  return (
+    <div>
+      <h1>Login Page</h1>
+      <p>This page will be built during Phase 2.</p>
+    </div>
+  )
+}
+
+export default Login
